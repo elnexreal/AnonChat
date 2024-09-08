c852304f4d6b2dd04adaad943b778ce72eb63612
elnexreal <40328573+elnexreal@users.noreply.github.com>
2024-09-08 04:02:39 -0300

    aight, basic server working, time to sleep

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,58 @@
-export default function Main() {
-  return (
-    <main>
+"use client";
 
-    </main>
-  )
+import { useEffect, useState } from "react";
+import { socket } from "./lib/socket";
+
+export default function Home() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState("N/A");
+  const [message, setMessage] = useState<string>("no message lol")
+
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onConnect() {
+      setIsConnected(true);
+      setTransport(socket.io.engine.transport.name);
+
+      socket.io.engine.on("upgrade", (transport) => {
+        setTransport(transport.name);
+      });
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+      setTransport("N/A");
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("message", (message) => {
+      setMessage(message)
+    })
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  }, []);
+
+  function sendMessage() {
+    socket.send("If you didn't click the button it means it works, if you clicked the button it also works hah trollface  me")
+  }
+
+  return (
+    <div>
+      <p>Status: {isConnected ? "connected" : "disconnected"}</p>
+      <p>Transport: {transport}</p>
+      <hr />
+
+      <button onClick={sendMessage} className="p-2 bg-green-500">
+        Test message sending
+      </button>
+      <p>Message: {message}</p>
+    </div>
+  );
 }
---
+++ server/server.ts
@@ -0,0 +1,48 @@
+import { createServer } from "node:http";
+import { Server, Socket } from "socket.io";
+import next from "next";
+
+const hostname = "localhost";
+const port = 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev, hostname, port });
+
+const handler = app.getRequestHandler();
+
+let clients: Socket[] = []
+
+app.prepare().then(() => {
+	const httpServer = createServer(handler);
+
+	const io = new Server(httpServer);
+
+	io.on("connection", (client) => {
+		// Add the client to the clients array for global messaging
+		clients.push(client)
+
+		// Remove the client from the array (this is for better performance ig idk)
+		client.on("disconnect", (reason) => {
+			const disClient = clients.find(disClient => disClient === client)
+
+			if (disClient) {
+				const index = clients.indexOf(disClient)
+				clients.splice(index, 1)
+			}
+
+			console.log(`Client disconnected (${reason})`)
+		})
+
+		client.on("message", (message) => {
+			clients.forEach(client => {
+				client.send(message)
+			})
+		})
+	});
+
+	httpServer.once("error", (err) => {
+		console.error(err);
+		process.exit(1);
+	}).listen(port, () => {
+		console.log(`> Ready on http://${hostname}:${port}`);
+	});
+});
