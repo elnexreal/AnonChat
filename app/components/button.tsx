39ebb89bb2a8df3630c04bc2f6d40e067c47d45f
elnexreal <40328573+elnexreal@users.noreply.github.com>
2024-09-09 19:34:06 -0300

    custom button component

---
+++ app/components/button.tsx
@@ -0,0 +1,18 @@
+import { forwardRef, ReactNode, Ref } from "react"
+
+interface ButtonProps {
+  children?: ReactNode
+  className?: string
+  onClick?: () => void
+}
+
+export default function Button({ ...props }: ButtonProps) {
+  return (
+    <button
+      className={`p-2 bg-white/5 hover:bg-white/10 rounded-lg transition-all ${props.className}`}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  )
+}
